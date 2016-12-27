63be3ff5a1ef1e685e1843e047881a899eed70fc
user <user@DESKTOP-8V1G4FA>
2016-12-27 12:38:04 +0800

    添加项目文件。

---
+++ BasicMmethodExtensionWeb/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BasicMmethodExtensionWeb.MvcApplication" Language="C#" %>
---
+++ BasicMmethodExtensionWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BasicMmethodExtensionWeb.Startup))]
+namespace BasicMmethodExtensionWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
